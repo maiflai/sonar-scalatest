5f2bd5f76c9f75800fe684d0d7de2bef65eb8ab1
Stu <maiflai@nospam>
2016-04-14 09:22:42 +0100

    verify toString

--- plugin/src/test/scala/com/github/maiflai/sonar/ScalaTestSensorTest.scala
+++ plugin/src/test/scala/com/github/maiflai/sonar/ScalaTestSensorTest.scala
@@ -1,11 +1,17 @@
 package com.github.maiflai.sonar
 
+import java.nio.file.Paths
+
 import com.github.maiflai.sonar.Result.{Fail, Pass, Skipped}
 import com.github.maiflai.sonar.ScalaTestSensor._
 import com.github.maiflai.sonar.ScalaTestSensorTest._
 import org.scalacheck.Gen
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{Matchers, WordSpec}
+import org.sonar.api.batch.fs._
+import org.sonar.api.batch.fs.internal.DefaultFileSystem
+import org.sonar.api.component.{Component, Perspective, ResourcePerspectives}
+import org.sonar.api.resources.Resource
 
 class ScalaTestSensorTest extends WordSpec with PropertyChecks with Matchers {
 
@@ -43,9 +49,26 @@ class ScalaTestSensorTest extends WordSpec with PropertyChecks with Matchers {
     }
   }
 
+  "toString" should {
+    "contain only alphabetical characters as it is used by the logger" in {
+      new ScalaTestSensor(UnimplementedResourcePerspectives, CurrentFileSystem).toString should fullyMatch regex "[a-zA-Z]+"
+    }
+  }
+
 }
 
 object ScalaTestSensorTest {
+
+  //noinspection NotImplementedCode
+  private object UnimplementedResourcePerspectives extends ResourcePerspectives {
+    override def as[P <: Perspective[_ <: Component]](perspectiveClass: Class[P], resource: Resource): P = ???
+
+    override def as[P <: Perspective[_ <: Component]](perspectiveClass: Class[P], inputPath: InputPath): P = ???
+
+    override def as[P <: Perspective[_ <: Component]](perspectiveClass: Class[P], component: Component): P = ???
+  }
+  private def CurrentFileSystem = new DefaultFileSystem(Paths.get("."))
+
   val anyFail = for {
     m <- Gen.identifier
     s <- Gen.identifier
